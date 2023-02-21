e34ad809fe3f99ac6d59bfa8cdd0e0774780de7b
devsince2021 <dev.ryu2021@gmail.com>
2023-02-21 09:56:25 +0900

    Create simple db connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hi");
+require("dotenv").config();
+import { createConnection } from "typeorm";
+
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: process.env.HOST,
+      port: Number(process.env.DB_PORT ?? 0),
+      username: process.env.USERNAME,
+      password: undefined,
+      database: process.env.DATABASE,
+    });
+
+    console.log("connected!");
+  } catch (err) {
+    console.error(err);
+    throw new Error("unable to connect protgres");
+  }
+};
+
+main();
